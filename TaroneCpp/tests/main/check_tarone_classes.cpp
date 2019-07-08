d98edffa1feaad5eac2be90362453176fa353963
Anja Gumpinger <anja.gumpinger@bsse.ethz.ch>
2019-07-08 13:20:33 +0200

    improved testing

--- TaroneCpp/tests/main/check_tarone_classes.cpp
+++ TaroneCpp/tests/main/check_tarone_classes.cpp
@@ -2,7 +2,7 @@
 * @Author: guanja
 * @Date:   2019-07-04 17:19:08
 * @Last Modified by:   guanja
-* @Last Modified time: 2019-07-05 16:40:11
+* @Last Modified time: 2019-07-08 12:18:57
 */
 
 // Include standard libs.
@@ -39,55 +39,17 @@ int main(int argc, char** argv)
   // Init tarone.
   TaroneCMH tarone(alpha, samples, cases);
 
-  std::cout << tarone.delta_t() << std::endl;
-  std::cout << tarone.corr_threshold() << std::endl;
-  std::cout << tarone.n_testable << std::endl;
+  std::vector<double> minpv_vec{ 0.001, 0.99, 0.15, 0.03, 0.02, 0.5, 0.75, 
+                                 0.005, 0.0000069, 0.12, 0.002, 0.0045, 
+                                 0.870964};
 
-  tarone.process_testable(0.001);
-  tarone.process_testable(0.99);
-  tarone.process_testable(0.15);
-  int k = 0;
-  while (k < 10)
+  for (int i=0; i<minpv_vec.size(); i++)
   {
-    tarone.process_testable(0.3);
-    std::cout << tarone.delta_t() << std::endl;
-    k +=1 ;
+    std::cout << "Processing min-pvalue = " << minpv_vec[i] << ". ";
+    std::cout << "Thresh. before proc.: delta_t = " << tarone.delta_t() << ". ";
+    tarone.process_testable(minpv_vec[i]);
+    std::cout << "Thresh. after proc.: delta_t = " << tarone.delta_t() << ". ";
+    std::cout << "n_testable = " << tarone.n_testable() << ". " << std::endl;
   }
-  
-
-  
-  std::cout << tarone.corr_threshold() << std::endl;
-  std::cout << tarone.n_testable << std::endl;
-
-  // std::string edge_file = "../../examples/data/edges.txt";
-  // std::string map_file = "../../examples/data/mapping_file.txt";
-
-  // std::vector<std::string> snp_order = {"rs1", "rs2", "rs3", "rs4", "rs5",
-  //                                       "rs6", "rs7", "rs8", "rs9", "rs10"};
-
-  // std::cout << "-----------------------------------" << std::endl;                                        
-  // std::cout << "EDGES" << std::endl << std::endl;
-
-  // Edges edge_class(edge_file);
-  // std::cout << "# nodes: " << edge_class.n_nodes << std::endl;
-  // std::cout << "# edges: " << edge_class.n_edges << std::endl;
-  // std::cout << "1. Egdes with string names ";
-  // std::cout << "(no duplicates/symmetries, no self-loops." << std::endl;
-  // print_nested_vector(edge_class.edges_str);
-  // std::cout << "2. Egdes with integer names " << std::endl;;
-  // print_nested_vector(edge_class.edges_int);
-
-  // std::cout << "-----------------------------------" << std::endl;
-  // std::cout << "MAPPING:" << std::endl << std::endl;
-  // Mapping mapping(map_file);
-  // std::cout << "1. Mapping with SNP-IDs." << std::endl;
-  // print_map_vector(mapping.geneview_map_str);
-
-  // // translate the mapping.
-  // mapping.translate_snp_ids(snp_order);
-  // std::cout << "2. SNP ordering in data set." << std::endl;
-  // print_vector(snp_order);
-  // std::cout << "3. Mapping with SNP-indices in ordering vector." << std::endl;
-  // print_map_vector(mapping.geneview_map_idx);
 
 }
