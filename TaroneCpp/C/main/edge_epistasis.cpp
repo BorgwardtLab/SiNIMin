6969dc58da0e3d7b35da67f1616ff2769d677a9d
Anja Gumpinger <anja.gumpinger@bsse.ethz.ch>
2019-07-09 18:23:48 +0200

    from scratch

--- TaroneCpp/C/main/edge_epistasis.cpp
+++ TaroneCpp/C/main/edge_epistasis.cpp
@@ -2,7 +2,7 @@
 * @Author: guanja
 * @Date:   2019-07-04 17:19:08
 * @Last Modified by:   guanja
-* @Last Modified time: 2019-07-08 19:02:55
+* @Last Modified time: 2019-07-09 18:23:27
 */
 
 // Include standard libs.
@@ -16,48 +16,16 @@
 #include <set>
 
 
+#include "../struct/data.cpp"
 #include "../struct/edges.cpp"
 #include "../struct/mapping.cpp"
+#include "../struct/tarone_cmh.cpp"
+#include "../struct/tarone_cmh_wy.cpp"
 #include "../utils/utils.cpp"
 
 
 
 int main(int argc, char** argv)
 {
-
-  std::string edge_file = "../examples/data/edges.txt";
-  std::string map_file = "../examples/data/mapping_file.txt";
-
-  std::vector<std::string> snp_order = {"rs1", "rs2", "rs3", "rs4", "rs5",
-                                        "rs6", "rs7", "rs8", "rs9", "rs10"};
-
-  // Read the edges.                                      
-  Edges edge_class(edge_file);
-
-  // Read the mapping from SNPs to genes.
-  Mapping mapping(map_file);
-
-  // translate the mapping to the SNP-IDs that are provided with the data set.
-  mapping.translate_snp_ids(snp_order);
-
-
-  // Iterate the edges.
-
-  /* For every edge, enumerate the intervals in a breadth-first, i.e.
-
-    1. Enumerate all intervals of length '1' and check, if they are testable.
-    2. Grow those intervals that were testable and compute their supports by
-       combination, i.e. for interval (tau, length), combine its support with 
-       the support of interval (tau-1, length). (Not sure whether this is the
-       best approach though, maybe brute-force would also work). Using an
-       unordered map with tuple (start, length) as key, support (complete, not 
-       per table, as this would not work for WY permutations) as value
-       would be good.
-    3. For every edge, check whether intervals were already enumerated.
-    4. Do a pairwise test of the supports of all possible interval combinations.
-  */
-
-
-
 
 }
