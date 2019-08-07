efe12059b8cf34b28fba73493077581dce7b9be0
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2019-08-07 18:51:43 +0200

    Skeleton for interval class
    Not yet tested, but the basic functionality should be there. Need to
    write simple test program still.

---
+++ TaroneCpp/C/struct/interval.hpp
@@ -0,0 +1,40 @@
+#ifndef INTERVAL_HPP__
+#define INTERVAL_HPP__
+
+#include <functional>
+
+class Interval
+{
+public:
+  using value_type = int;
+
+  Interval() = default;
+
+  Interval(value_type start, value_type end)
+    : _start(start)
+    , _end(end)
+  {
+  }
+
+  value_type start() const { return _start; }
+  value_type end() const { return _end; }
+
+private:
+  value_type _start = 0;
+  value_type _end = 0;
+};
+
+namespace std
+{
+  template<> struct hash<Interval>
+  {
+    using value_type = Interval::value_type;
+
+    std::size_t operator()(const Interval& interval)
+    {
+      return hash<value_type>(interval.start()) ^ hash<value_type>(interval.end());
+    }
+  };
+}
+
+#endif
