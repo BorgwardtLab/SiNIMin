d1aa9853330f05f232ce9bab78afcd8d24a6fccd
Anja Gumpinger <guanja@bsse-bs-dock-dhcp-385.ethz.ch>
2019-09-17 13:02:37 +0200

    minor

--- TaroneCpp/examples/runs/athal/avrB_data_sinimin.sh
+++ TaroneCpp/examples/runs/athal/avrB_data_sinimin.sh
@@ -1,12 +1,12 @@
 # @Author: guanja
 # @Date:   2019-08-06 20:49:46
 # @Last Modified by:   guanja
-# @Last Modified time: 2019-08-09 18:45:04
+# @Last Modified time: 2019-09-17 13:01:11
 
 
 ROOT="/home/guanja/projects/cpkgs/TaroneCpp/TaroneCpp"
 
-EXEC="${ROOT}/compiled/edge_epistasis"
+EXEC="${ROOT}/compiled/sinimin"
 
 DAT_FN="${ROOT}/examples/data_athal/avrB_X.txt"
 COV_FN="${ROOT}/examples/data_athal/avrB_covar_n2.txt"
@@ -15,15 +15,10 @@ LAB_FN="${ROOT}/examples/data_athal/avrB_Y.txt"
 MAP_FN="${ROOT}/examples/data_athal/avrB_snp_map.txt"
 SNP_FN="${ROOT}/examples/data_athal/avrB_snpID.txt"
 
-N_THREADS=20
+N_THREADS=10
 
-timestamp=$(date +%s)
-
-OUT_PR="${ROOT}/examples/output_athal/siniminWY/"
-mkdir -p ${OUT_PR}
-OUT_PR+="siniminWY_avrB_${timestamp}"
-
-
+OUT_PR="${ROOT}/examples/output/athal" 
+mkdir -p "${OUT_PR}"
 
 "${EXEC}" \
   -i "${DAT_FN}" \
@@ -33,6 +28,5 @@ OUT_PR+="siniminWY_avrB_${timestamp}"
   -e "${EDG_FN}" \
   -s "${SNP_FN}" \
   -f 0.05 \
-  -p 1000 \
   -n "${N_THREADS}" \
-  -o "${OUT_PR}" \
+  -o "${OUT_PR}/sinimin_avrB" \
--- TaroneCpp/examples/runs/athal/avrB_data_siniminWY.sh
+++ TaroneCpp/examples/runs/athal/avrB_data_siniminWY.sh
@@ -1,12 +1,12 @@
 # @Author: guanja
 # @Date:   2019-08-06 20:49:46
 # @Last Modified by:   guanja
-# @Last Modified time: 2019-08-12 10:50:25
+# @Last Modified time: 2019-09-17 13:01:30
 
 
 ROOT="/home/guanja/projects/cpkgs/TaroneCpp/TaroneCpp"
 
-EXEC="${ROOT}/compiled/edge_epistasis"
+EXEC="${ROOT}/compiled/sinimin"
 
 DAT_FN="${ROOT}/examples/data_athal/avrB_X.txt"
 COV_FN="${ROOT}/examples/data_athal/avrB_covar_n2.txt"
@@ -15,15 +15,10 @@ LAB_FN="${ROOT}/examples/data_athal/avrB_Y.txt"
 MAP_FN="${ROOT}/examples/data_athal/avrB_snp_map.txt"
 SNP_FN="${ROOT}/examples/data_athal/avrB_snpID.txt"
 
-N_THREADS=5
+N_THREADS=10
 
-timestamp=$(date +%s)
-
-OUT_PR="${ROOT}/examples/output_athal/edgeEpiWY" 
+OUT_PR="${ROOT}/examples/output/athal" 
 mkdir -p "${OUT_PR}"
-OUT_PR+="/edgeEpiWY_avrB_${timestamp}"
-
-
 
 "${EXEC}" \
   -i "${DAT_FN}" \
@@ -36,4 +31,4 @@ OUT_PR+="/edgeEpiWY_avrB_${timestamp}"
   -d 1 \
   -p 1000 \
   -n "${N_THREADS}" \
-  -o "${OUT_PR}" \
+  -o "${OUT_PR}/siniminWY_avrB" \
